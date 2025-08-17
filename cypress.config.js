8d0488ae7078be30acc3ba0421b48976708ef18a
Tetja-na <tanjagrunertt@gmail.com>
2025-08-17 17:59:00 +0200

    letztes

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "q69n2d",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
