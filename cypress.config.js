4cc968ec87895ff8d0d3563d06f460c84c51dc1d
Tetja-na <tanjagrunertt@gmail.com>
2025-08-31 18:30:01 +0200

    HWeinundzwanzig

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: true,
+    html: true,
+    json: true
+  },
   e2e: {
-    projectId: "q69n2d",
-    baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config;
     },
   },
 });
